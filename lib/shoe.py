48b24044fb350a2e997d30fddf682823ade3b0d2
shmaphilo <philipogaye062@gmail.com>
2023-09-04 02:28:38 -0400

    Assignment Done

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
-
+ 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        self.size = size
+        self.condition = "New"
+
+    def repair(self):
+        print("The shoe has been repaired.")
+        self.condition = "New"
